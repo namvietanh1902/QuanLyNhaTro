94f93f208f0eebb75727775508a885c927a3b748
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2022-05-21 13:06:38 +0700

    1 phan cua entity

---
+++ EF/Model/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyNhaTro.EF.Model
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Tên khách trọ không được để trống")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        DateTime Birthday { get; set; }
+        [Required(ErrorMessage ="Giới tính không được để trống")]
+        public bool Gender { get; set; }
+        [Required(ErrorMessage="CMND không được để trống")]
+        public string CMND { get; set; }
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        public string SDT { get; set; }
+        [Required(ErrorMessage = "Nghề nghiệp không được để trống")]
+        public string Job { get; set; }
+        
+    
+        public bool isDelete { get; set; } = false;
+        public virtual Contract Contract;
+
+
+    }
+}
---
+++ EF/Model/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyNhaTro.EF.Model
+{
+    public class Room
+    {   [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Tên phòng không được để trống")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Số lượng không được để trống")]
+        [Range(1,int.MaxValue,ErrorMessage ="Số lượng người ở phải từ 1 người trở lên ")]
+        public int Capacity { get; set; }
+        [Required(ErrorMessage ="Giá phòng không được để trống")]
+        [Range(0,int.MaxValue,ErrorMessage ="Gía phòng không được là giá trị âm")]
+        public int Price { get; set; }
+        public bool isRent { get; set; } = false;
+        public bool isDelete { get; set; } = false;
+        public virtual ICollection<Contract> Contracts { get; set; }
+    }
+}
