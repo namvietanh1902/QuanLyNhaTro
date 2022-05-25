c77fa3ebfb090b4953c4f6aed45b7d7f8cb0a4cd
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2022-05-25 12:07:56 +0700

    xong search va sort room

---
+++ BLL/BLL_Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaTro.BLL
+{
+     public class BLL_Service
+    {
+        private static BLL_Service _instance;
+
+        public static BLL_Service Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BLL_Service();
+                }
+            return _instance;
+            }
+            private set { }
+        }
+
+    }
+}
